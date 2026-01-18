2ffdef965f00a51657f2d7d98c8bf6a771bd5d75
nobody_on_earth <reddeadarthurfan1889@gmail.com>
2026-01-18 13:01:11 -0600

    Update mmd.js

--- mmd.js
+++ mmd.js
@@ -1,10 +1,24 @@
-/*
-Before we start, the command must be an html element
-Syntax:
-Command$ Text
-*/
-function findcommname(line) {
-  let = line.split('$')
-  let comm = line[0]
-  return comm;
-}
+// command $ text
+function gethtml(line) {
+  function getcommname(line) {
+    let commname = line.split('$')[0];
+    return commname;
+  }
+   function gettextfromline(line) {
+     let text = line.split('$')[1];
+     return text;
+   }
+   function mmdtohtml(commname, text) {
+    let begintag = '<' + commname + '>';
+    let endtag = '</' + commname + '>';
+    let html = begintag + text + endtag;
+    return html;
+  }
+  function formatted(line) {
+    let command = getcommname(line);
+    let content = gettextfromline(line);
+    let result = mmdtohtml(command, content);
+    return result;
+    }
+    return formatted(line);
+ }
